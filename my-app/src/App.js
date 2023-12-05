6e500a2903ed9dd2598549d80c734f9ba373f244
Eric casa <erictkurachi@gmail.com>
2023-12-04 23:44:43 -0300

    Added title and about

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,19 @@ import SocialMediaBar from './SocialMediaBar';
 const App = () => {
   return (
     <div>
+      <h1>Eric Kurachi</h1>
       <PortfolioHeader />
       {/* Other components/content of portfolio */}
+      <h2>About</h2>
+      <p>
+        Software developer with 2 years of professional
+        experience. Created and optimized web apps and
+        automation solutions assisting around 70 R&D
+        technicians and engineers. Developed several
+        projects aimed at teaching programming and
+        artificial intelligence, impacting hundreds of
+        students.
+      </p>
       <SocialMediaBar />
       © 2023 Eric Kurachi.
     </div>
